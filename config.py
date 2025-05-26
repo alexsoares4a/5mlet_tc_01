066c8f146ccde82ee47e66a4a2c9014d5a97e02d
alexsoares4a <alexssonline@gmail.com>
2025-05-26 16:47:59 -0300

    Removida as variáveis de ambiente de configuração SMTP

--- config.py
+++ config.py
@@ -3,16 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "sua_chave_secreta")
+# Configurações da API
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
-SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
-SMTP_PORT = os.getenv("SMTP_PORT", 587)
-SMTP_USER = os.getenv("SMTP_USER", "univesp.proj@gmail.com")
-SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", "hxeg gxnx ljpi mupq")
-
-SMTP_CONFIG = {
-    "host": "smtp.gmail.com",  # Para Gmail
-    "port": 465,
-    "user": "univesp.proj@gmail.com",
-    "password": "hxeg gxnx ljpi mupq"  # Use App Password se tiver 2FA
-}
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
